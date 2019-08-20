c5f859081359ec3ab5c5e14b03c9d348416d4a4e
Vijay Thayalan <vijaythayalanselvaraj@gmail.com>
2019-08-20 17:37:59 +0530

    Update DateSelector.js

--- src/DateSelector.js
+++ src/DateSelector.js
@@ -6,40 +6,62 @@ class DateSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: new Date()
+      startDate: new Date(),
+	  amount: ""
     };
+	this.onChange = this.onChange.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
- 
+
   handleChange(date) {
     this.setState({
       startDate: date
     });
   }
+
+  onChange(e) {
+    const re = /^[0-9\b]+$/;
+    if ((e.target.value === '' || re.test(e.target.value)) && e.target.value.length <= 5) {
+        this.setState({amount: e.target.value})
+    }
+  }
  
+  getMonthFromString = function (monthName){
+    var date = new Date(Date.parse(monthName + 1, new Date().getFullYear()));
+    var firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDate();
+    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    var month = new Date(Date.parse(monthName + 1, new Date().getFullYear())).getMonth()+1;
+    var year = new Date().getFullYear();
+
+       return firstDay + "-" + month + "-" + year + " - " + lastDay + "-" + month + "-" + year;
+	}
+
   render() {
     return (
 	  this.props.isHeader ?
-	  <div class="column">
-	  <p>Date</p>
-	  <p>End of Day Amount</p>
+	  <div class="DetailsDiv">
+		  <label>Date</label>
+		  <label>End of Day Amount ₹ </label>
+		  <label>Add/Remove</label>
 	  </div>
 	  :
-	  this.props.isFirst ?
-	  <div class="column">
-      <DatePicker
+	  this.props.isFirst === "true" ?
+	  <div class="DetailsDiv">
+      <DatePicker className="datepick"
         selected={this.state.startDate}
         onChange={this.handleChange}
       />
-	  <p>+</p>
+	  <input type="text" pattern='[0-9]{0,5}' value={this.state.amount} onChange={this.onChange}/>
+	  <button type="button">+</button>
 	  </div>
 	  :
-	  <div class="column">
-	  <DatePicker
+	  <div class="DetailsDiv">
+	  <DatePicker className="datepick"
         selected={this.state.startDate}
         onChange={this.handleChange}
       />
-	  <p>-</p>
+	  <input type="text" pattern='[0-9]{0,5}' value={this.state.amount} onChange={this.onChange}/>
+	  <button type="button">-</button>
 	  </div>
     );
   }
