0ea470f44b06df286b9c0574f2555371063a6eaa
Vijay Thayalan <vijaythayalanselvaraj@gmail.com>
2019-08-30 15:47:09 +0530

    Update DateSelector.js

--- src/DateSelector.js
+++ src/DateSelector.js
@@ -1,68 +1,216 @@
 import React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "./DateSelector.css";
 
 class DateSelector extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: new Date(),
-	  amount: ""
+      showBalance: false,
+      tableHeader: ["Date", "End of Day Amount ₹"],
+      tableData: [
+        {
+          key: "0",
+          selectionDate: new Date(),
+          selectionAmount: "0"
+        }
+      ]
     };
-	this.onChange = this.onChange.bind(this);
+    this.onChange = this.onChange.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.plus = this.plus.bind(this);
+    this.minus = this.minus.bind(this);
+    this.checkBalance = this.checkBalance.bind(this);
   }
 
-  handleChange(date) {
-    this.setState({
-      startDate: date
+  editEachRow(eachRow, keyIndex, newDate) {
+    return eachRow.map(item => {
+      var temp = Object.assign({}, item);
+      if (temp.key == keyIndex) {
+        temp.selectionDate = newDate;
+      }
+      return temp;
     });
   }
 
-  onChange(e) {
+  editEachRowAmount(eachRow, keyIndex, amount) {
+    return eachRow.map(item => {
+      var temp = Object.assign({}, item);
+      if (temp.key == keyIndex) {
+        temp.selectionAmount = amount;
+      }
+      return temp;
+    });
+  }
+
+  onChange(e, valE) {
+    const val = valE.target.value;
     const re = /^[0-9\b]+$/;
-    if ((e.target.value === '' || re.test(e.target.value)) && e.target.value.length <= 5) {
-        this.setState({amount: e.target.value})
+    if ((val === "" || re.test(val)) && val.length <= 5) {
+      let tableDa = this.editEachRowAmount(this.state.tableData, e.index, val);
+      this.setState({
+		  showBalance: false,
+		  tableData: tableDa
+      });
     }
   }
- 
-  getMonthFromString = function (monthName){
+
+  handleChange(event, date) {
+    let tableDa = this.editEachRow(this.state.tableData, event.index, date);
+    this.setState({
+	  showBalance: false,
+      tableData: tableDa
+    });
+  }
+
+  minus() {
+    let tableDa = this.state.tableData;
+    tableDa.pop();
+    this.setState({
+	  showBalance: false,
+      tableData: tableDa
+    });
+  }
+
+  plus() {
+    let tableDa = this.state.tableData;
+
+    tableDa.push({
+      key: this.state.tableData.length,
+      selectionDate: new Date(),
+      selectionAmount: "0"
+    });
+    this.setState({
+	  showBalance: false,
+      tableData: tableDa
+    });
+  }
+
+  checkBalance() {
+    console.log("MinBalance: " + this.props.minBalance);
+	const currentDate = new Date();
+	let currentDay = currentDate.getDay();
+    let tableDa = this.state.tableData;
+	const keyValue = [];
+
+	Object.entries(tableDa).forEach(function([key, value]) 
+	{
+		let keyVal = value["selectionDate"].getDate();
+		let valueVal = value["selectionAmount"];
+
+		keyValue.push({keyVal: valueVal});
+	});
+  }
+
+  getMonthFromString = function(monthName) {
     var date = new Date(Date.parse(monthName + 1, new Date().getFullYear()));
     var firstDay = new Date(date.getFullYear(), date.getMonth(), 1).getDate();
-    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-    var month = new Date(Date.parse(monthName + 1, new Date().getFullYear())).getMonth()+1;
+    var lastDay = new Date(
+      date.getFullYear(),
+      date.getMonth() + 1,
+      0
+    ).getDate();
+    var month =
+      new Date(Date.parse(monthName + 1, new Date().getFullYear())).getMonth() +
+      1;
     var year = new Date().getFullYear();
 
-       return firstDay + "-" + month + "-" + year + " - " + lastDay + "-" + month + "-" + year;
-	}
+    return (
+      firstDay +
+      "-" +
+      month +
+      "-" +
+      year +
+      " - " +
+      lastDay +
+      "-" +
+      month +
+      "-" +
+      year
+    );
+  };
+
+  renderTableData() {
+    return this.state.tableData.map((eachRow, index) => {
+      const { selectionDate, selectionAmount } = eachRow;
+      return (
+        <tr key={index}>
+          <td>
+            <DatePicker
+              id={index}
+              className="datepick"
+              selected={selectionDate}
+              onChange={this.handleChange.bind(this, {
+                index
+              })}
+            />
+          </td>
+          <td>
+            
+            <input
+              key={index}
+              id={index}
+              type="text"
+              pattern="[0-9]{0,5}"
+              value={selectionAmount}
+              onChange={this.onChange.bind(this, {
+                index
+              })}
+            />
+          </td>
+        </tr>
+      );
+    });
+  }
+
+  renderTableHeader() {
+    let header = Object.keys(this.state.tableHeader);
+
+    return header.map(key => {
+      return (
+        <th key={key} align="left">
+          
+          {this.state.tableHeader[key]}
+        </th>
+      );
+    });
+  }
 
   render() {
+    const isOnlyOneRow = this.state.tableData.length === 1;
+	const isShowBalance = this.state.showBalance;
+	
     return (
-	  this.props.isHeader ?
-	  <div class="DetailsDiv">
-		  <label>Date</label>
-		  <label>End of Day Amount ₹ </label>
-		  <label>Add/Remove</label>
-	  </div>
-	  :
-	  this.props.isFirst === "true" ?
-	  <div class="DetailsDiv">
-      <DatePicker className="datepick"
-        selected={this.state.startDate}
-        onChange={this.handleChange}
-      />
-	  <input type="text" pattern='[0-9]{0,5}' value={this.state.amount} onChange={this.onChange}/>
-	  <button type="button">+</button>
-	  </div>
-	  :
-	  <div class="DetailsDiv">
-	  <DatePicker className="datepick"
-        selected={this.state.startDate}
-        onChange={this.handleChange}
-      />
-	  <input type="text" pattern='[0-9]{0,5}' value={this.state.amount} onChange={this.onChange}/>
-	  <button type="button">-</button>
-	  </div>
+      <div>
+        <table>
+          <tbody>
+            <tr> {this.renderTableHeader()} </tr> {this.renderTableData()}
+          </tbody>
+        </table>
+        <div align="center">
+          <button disabled={isOnlyOneRow} type="button" onClick={this.minus}>
+            Remove
+          </button>
+          <button type="button" onClick={this.plus}>
+            Add
+          </button>
+        </div>
+        <div align="center">
+          <button
+            className="checkBalance"
+            onClick={this.checkBalance}
+            type="button"
+          >
+            Check
+          </button>
+		  
+		  {isShowBalance} ?
+		  <label>Your Account has </label>
+		  :
+		  null
+        </div>
+      </div>
     );
   }
 }
